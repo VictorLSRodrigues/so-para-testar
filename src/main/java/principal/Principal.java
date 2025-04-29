87c4b84f9bedf21c84a5de13ceff2f806de0a3f2
victo <victo@DESKTOP-UQ3GBG2>
2025-04-29 16:14:33 -0300

    add

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package principal;
 
 import javax.swing.JOptionPane;
 import model.Aluno;
 import model.Professor;
 
-/**
- *
- * @author Lorenzo
- */
+
 public class Principal {
 
     public static void main(String[] args) {
