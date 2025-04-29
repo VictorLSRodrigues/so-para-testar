3081e980cdf214fb3c8733ce143e5d6b5c891306
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-04-29 16:45:59 -0300

    add

--- src/test/java/model/AlunoTest.java
+++ src/test/java/model/AlunoTest.java
@@ -1,4 +1,12 @@
+
 package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AlunoTest {
     
